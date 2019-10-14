3146f8f9c9c3d6d625933cf39d938e5db182a6a9
Ebony Sharrock <ebonysharrock@gmail.com>
2019-10-14 17:29:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,6 +2,6 @@ def square_array(array)
     counter = 0
     while counter < array.size
       new_array[counter] = array[counter]**2
+      puts new_array
       counter += 1
-  puts new_array
 end
